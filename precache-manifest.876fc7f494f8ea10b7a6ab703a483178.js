eb36fe52e3d7806fa6540fdf5c0989d8df39c253
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-09-21 17:14:11 +0200

    Updates

---
+++ precache-manifest.876fc7f494f8ea10b7a6ab703a483178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ddb75c16f73241d2b9f",
+    "url": "/react-todo/static/css/main.5fe252e6.chunk.css"
+  },
+  {
+    "revision": "4ddb75c16f73241d2b9f",
+    "url": "/react-todo/static/js/main.4ddb75c1.chunk.js"
+  },
+  {
+    "revision": "3d3ecf74a190e5419781",
+    "url": "/react-todo/static/js/1.3d3ecf74.chunk.js"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "378897b86238b13ca57fba4e7a6841f8",
+    "url": "/react-todo/index.html"
+  }
+];
